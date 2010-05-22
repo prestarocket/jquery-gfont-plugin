7f2afe3812cb6cfc3a9c2175aeaedaa66be84315
Takamasa Aoi <tkms.aoi@gmail.com>
2010-05-23 03:20:45 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# jquery-gfont-plugin
+
+A simple jQuery plugin for [Google Font API](http://code.google.com/intl/ja/apis/webfonts/).
+
+
+## Usage
+
+    $('#foo').gfont('<Font Name>');
---
+++ jquery.gfont.js
@@ -0,0 +1,32 @@
+/**
+ * jQuery Google Font API Plugin
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+(function($) {
+    var cache = [];
+
+    $.gfont = function() {
+        var family = $.map(arguments, function(value) {
+            if ($.inArray(value, cache) === -1) {
+                cache.push(value);
+                return encodeURI(value.replace(/ /g, '+'));
+            } else {
+                return null;
+            }
+        }).join('|');
+        if (family) {
+            $('head').eq(0).prepend('<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=' + family + '" />');
+        }
+    };
+
+    $.fn.gfont = function() {
+        $.gfont.apply(null, arguments);
+        var family = $.map(arguments, function(value) { return '"' + value + '"'; }).join(', ');
+        return this.each(function() {
+            var tmp = $(this).css('font-family');
+            $(this).css('font-family', family + (tmp ? ', ' + tmp : ''));
+        });
+    };
+})(jQuery);
